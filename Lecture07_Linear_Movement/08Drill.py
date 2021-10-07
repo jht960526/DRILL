927d81363a1a4c7128a55a7bd592ceb8e2c1447a
RedTak <jht960526@naver.com>
2021-10-07 15:58:26 +0900

    DRILL #08 완성
    2018180052정홍택
    파라미터 표현을 이용한 그림 그리기
    https://www.youtube.com/watch?v=izBKHWyERLQ

---
+++ Lecture07_Linear_Movement/08Drill.py
@@ -0,0 +1,77 @@
+import turtle
+import random
+import math
+
+
+def stop():
+    turtle.bye()
+
+
+def prepare_turtle_canvas():
+    turtle.setup(1024, 768)
+    turtle.bgcolor(0.2, 0.2, 0.2)
+    turtle.penup()
+    turtle.hideturtle()
+    turtle.shape('arrow')
+    turtle.shapesize(2)
+    turtle.pensize(5)
+    turtle.color(1, 0, 0)
+    turtle.speed(100)
+    turtle.goto(-500, 0)
+    turtle.pendown()
+    turtle.goto(480, 0)
+    turtle.stamp()
+    turtle.penup()
+    turtle.goto(0, -360)
+    turtle.pendown()
+    turtle.goto(0, 360)
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.penup()
+    turtle.home()
+
+    turtle.shape('circle')
+    turtle.pensize(1)
+    turtle.color(0, 0, 0)
+    turtle.speed(50)
+
+    turtle.onkey(stop, 'Escape')
+    turtle.listen()
+
+
+def draw_big_point(p):
+    turtle.goto(p)
+    turtle.color(0.8, 0.9, 0)
+    turtle.dot(15)
+    turtle.write('     ' + str(p))
+
+
+def draw_point(p):
+    turtle.goto(p)
+    turtle.dot(5, random.random(), random.random(), random.random())
+
+
+
+def draw_circle(p1, p2):
+    #draw_big_point(p1)
+    #draw_big_point(p2)
+
+    x1, y1 = p1
+    x2, y2 = p2
+
+    for i in range(0, 80):
+        t = i / (2*math.pi)
+        x = (50 - 100) * math.cos(t) + 100 * math.cos(t * (0.50 - 1))
+        y = (50 - 100) * math.sin(t) - 100 * math.sin(t * (0.50 - 1))
+        draw_point((x, y))
+
+    draw_point(p2)
+
+
+prepare_turtle_canvas()
+
+p1 = 100, 20
+p2 = 200, 300
+
+draw_circle(p1, p2)
+turtle.done()
