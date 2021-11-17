30558b422dbd283d41336d20fa08d966ae80e158
RedTak <jht960526@naver.com>
2021-11-18 06:02:51 +0900

    Add Check Bottom Collision

--- 2DGameProject_Mario/Main_State.py
+++ 2DGameProject_Mario/Main_State.py
@@ -19,12 +19,13 @@ def collide(a, b):
     left_a, bottom_a, right_a, top_a = a.get_collision()
     left_b, bottom_b, right_b, top_b = b.get_collision()
 
-    if left_a > right_b: return False
-    if right_a < left_b: return False
-    if top_a < bottom_b: return False
-    if bottom_a > top_b: return False
+    #if left_a > right_b: return False
+    #if right_a < left_b: return False
+    #if top_a < bottom_b: return False
+    if bottom_a > top_b: return True
+    #if bottom_a < top_b: return True
 
-    return True
+    #return False
 
 
 def enter():
