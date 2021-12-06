6c74b01466ae0d80ab2c7ecf7189ccf1ea3db351
RedTak <jht960526@naver.com>
2021-12-07 04:47:08 +0900

    Fix Brick Scrolling

--- 2DGameProject_Mario/Brick.py
+++ 2DGameProject_Mario/Brick.py
@@ -1,9 +1,10 @@
 import game_framework
+import server
 
 from pico2d import *
 
 PIXEL_PER_METER = (30.0/0.3)
-RUN_SPEED_KMPH = 13.0
+RUN_SPEED_KMPH = 12.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
@@ -16,36 +17,41 @@ class Brick:
     image = None
     def __init__(self):
         self.x, self.y = 300, 230
+        self.cx, self.cy = self.x, self.y
+        print('brick')
 
         if Brick.image == None:
             Brick.image = load_image('Resource/brick1.png')
 
     def update(self):
+        self.cx, self.cy = self.x - server.backGround.window_left, self.y - server.backGround.window_bottom
         pass
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.image.draw(self.cx, self.cy)
         draw_rectangle(*self.get_collision())
 
     def get_collision(self):
-        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+        return self.cx - 20, self.cy - 20, self.cx + 20, self.cy + 20
 
 class Brick_Q:
     image = None
 
     def __init__(self):
         self.x, self.y = 350, 230
+        self.cx, self.cy = self.x, self.y
         self.frame = 0
 
         if Brick_Q.image == None:
             Brick_Q.image = load_image('Resource/Brick_Q.png')
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 40, 0, 40, 30, self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 40, 0, 40, 30, self.cx, self.cy)
         draw_rectangle(*self.get_collision())
 
     def update(self):
+        self.cx, self.cy = self.x - server.backGround.window_left, self.y - server.backGround.window_bottom
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
 
     def get_collision(self):
-        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+        return self.cx - 20, self.cy - 20, self.cx + 20, self.cy + 20
